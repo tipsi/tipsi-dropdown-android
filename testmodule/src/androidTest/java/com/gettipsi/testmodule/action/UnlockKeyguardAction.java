07a208e290aeee832ffa72388d0cefe03dd042a3
dmitriy <dmitriy.malec@gmail.com>
2016-09-15 23:28:21 +0300

    Fix UnlockKeyguardActiond

--- testmodule/src/androidTest/java/com/gettipsi/testmodule/action/UnlockKeyguardAction.java
+++ testmodule/src/androidTest/java/com/gettipsi/testmodule/action/UnlockKeyguardAction.java
@@ -6,6 +6,8 @@ import android.support.test.espresso.ViewAction;
 import android.view.View;
 import android.view.WindowManager;
 
+import com.gettipsi.tpsdropdown.Dropdown;
+
 import org.hamcrest.Matcher;
 
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
@@ -14,7 +16,7 @@ public class UnlockKeyguardAction implements ViewAction {
 
     @Override
     public Matcher<View> getConstraints() {
-        return isAssignableFrom(View.class);
+        return isAssignableFrom(Dropdown.class);
     }
 
     @Override
